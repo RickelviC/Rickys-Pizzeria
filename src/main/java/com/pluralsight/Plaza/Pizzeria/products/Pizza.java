f2aefb04adebda334d1d8bdbdb4b6f694c989ef5
RickelviC <Cruzrickelvi@gmail.com>
2026-05-22 14:37:11 -0400

    added the addtopping method to Pizza.java

--- src/main/java/com/pluralsight/Plaza/Pizzeria/products/Pizza.java
+++ src/main/java/com/pluralsight/Plaza/Pizzeria/products/Pizza.java
@@ -10,6 +10,49 @@ public class Pizza implements IPriceable{
     private boolean stuffed;
     private String crustType;
 
+    public Pizza(List<Topping> toppings, int size, boolean stuffed, String crustType) {
+        this.toppings = toppings;
+        this.size = size;
+        this.stuffed = stuffed;
+        this.crustType = crustType;
+    }
+
+    public List<Topping> getToppings() {
+        return toppings;
+    }
+
+    public void setToppings(List<Topping> toppings) {
+        this.toppings = toppings;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public boolean isStuffed() {
+        return stuffed;
+    }
+
+    public void setStuffed(boolean stuffed) {
+        this.stuffed = stuffed;
+    }
+
+    public String getCrustType() {
+        return crustType;
+    }
+
+    public void setCrustType(String crustType) {
+        this.crustType = crustType;
+    }
+
+    public void AddTopping(Topping topping){
+        toppings.add(topping);
+    }
+
     @Override
     public double getPrice() {
         if (size == 8) {
