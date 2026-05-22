4df9dfea7a3de98b18e8db29ad6b9484e9d84330
RickelviC <Cruzrickelvi@gmail.com>
2026-05-22 14:59:08 -0400

    added user input to main for option to pick location

--- src/main/java/com/pluralsight/Plaza/Main.java
+++ src/main/java/com/pluralsight/Plaza/Main.java
@@ -2,9 +2,40 @@ package com.pluralsight.Plaza;
 
 import com.pluralsight.Plaza.Pizzeria.main.UserInterface;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        UserInterface ui = new UserInterface();
-        ui.display();
+        Scanner scanner = new Scanner(System.in);
+
+        boolean quit = false;
+        while (!quit) {
+            System.out.println("where would you like to place an order at today?");
+            System.out.println();
+            System.out.println("1) Ricky's Pizzeria");
+            System.out.println("4. to quit");
+            System.out.print("Enter your choice: ");
+            System.out.println();
+            String choice = scanner.nextLine();
+
+            switch (choice) {
+                case "1":
+                    UserInterface ui = new UserInterface();
+                    ui.display();
+                    break;
+                case "2":
+
+                    break;
+                case "3":
+
+                    break;
+                case "4":
+                    quit = true;
+                    break;
+                default:
+                    System.out.println("that's not a real place here");
+            }
+        }
+        scanner.close();
     }
 }
